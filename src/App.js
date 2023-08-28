85daf8514e44ec7552b496e7a4e9bcfc7c5e9440
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-28 14:38:32 -0700

    added bars, but dont work exactly right

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import {Audio} from 'react-loader-spinner'
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<Audio
+        type="Bars"
+        color="#ff00ff"
+        height={200}
+        width= {200}
+        style={{margin:"o auto"}}/>}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
